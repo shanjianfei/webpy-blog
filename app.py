634756c5d8f05293afaeb7ad70d13db17dc7e0cb
Stephen <18862238016@163.com>
2018-04-21 23:41:03 +0800

    implement login and register method
    Signed-off-by: Stephen <18862238016@163.com>

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import web
+import json
+
+users = {}
 
 urls = (
 	'/', 'Index',
-	'/login', 'Login'
+	'/login', 'Login',
+	'/register', 'Register'
 )
 
 app = web.application(urls, globals())
@@ -16,7 +20,24 @@ class Login(object):
 		web.seeother('/static/html/login.html')
 
 	def POST(self):
+		data = web.input()
+		user = data['usr']
+		password = data['psd']
+		if user in users.keys() and users[user] == password:
+			web.seeother('/')
+		else:
+			return json.dumps({'status': 'fail'})
+
 		return 'login success'
+
+class Register(object):
+	def POST(self):
+		data = web.input()
+		if 'usr' in data and 'psd' in data:
+			users[data['usr']] = data['psd']
+			return json.dumps({'status': 'success'})
+		else:
+			return json.dumps({'status': 'fail'})
 
 if __name__ == '__main__':
 	app.run()
