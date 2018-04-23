d50e02e17751c046084783967e800df698a23004
Stephen <18862238016@163.com>
2018-04-23 16:09:11 +0800

    添加主页面
    Signed-off-by: Stephen <18862238016@163.com>

--- app.py
+++ app.py
@@ -6,29 +6,37 @@ users = {}
 urls = (
 	'/', 'Index',
 	'/login', 'Login',
-	'/register', 'Register'
+	'/register', 'Register',
+	'/getUser', 'GetUser'
 )
 
+web.config.debug = False
 app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore('sessions'), {'username': None, 'login': 0})
+
 
 class Index(object):
 	def GET(self):
-		return 'hello'
+		if session.get('login') == 1:
+			raise web.seeother('/static/html/index_login.html')
+		else:
+			raise web.seeother('/static/html/index_not_login.html')
 
 class Login(object):
 	def GET(self):
-		web.seeother('/static/html/login.html')
+		raise web.seeother('/static/html/login.html')
 
 	def POST(self):
 		data = web.input()
 		user = data['usr']
 		password = data['psd']
 		if user in users.keys() and users[user] == password:
-			web.seeother('/')
+			session.login = 1
+			session.username = user
+			web.setcookie('username', user, 3600)
+			return json.dumps({'status': 'success'})
 		else:
 			return json.dumps({'status': 'fail'})
-
-		return 'login success'
 
 class Register(object):
 	def POST(self):
@@ -38,6 +46,12 @@ class Register(object):
 			return json.dumps({'status': 'success'})
 		else:
 			return json.dumps({'status': 'fail'})
+
+class GetUser(object):
+	def GET(self):
+		if session.get('username', None) is not None and session.get('login', 0) == 1:
+			return json.dumps({'username': session.get('username')})
+
 
 if __name__ == '__main__':
 	app.run()
